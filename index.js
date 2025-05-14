e2223f1a48c9e699e6650757ee03416c9898a6e2
Sam W <samevwro@gmail.com>
2025-05-14 11:28:28 -0600

    desktop Push

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-
-
 var X = 1;
 var O = 2;
 let turn = X;
 let turnNum = 1;
-let gameStatus = '';
-
+let gameStatus = 'play';
+var clickEvent = 0
 const XSpot = '❌';
 const OSpot = '⭕';
 
@@ -23,7 +21,7 @@ function spotSelection(id) {
             document.getElementById(id).innerHTML = XSpot;
             gameGrid[id] = X;
         }else {
-            document.getElementById(id).p.innerHTML = OSpot;
+            document.getElementById(id).innerHTML = OSpot;
             gameGrid[id] = O;
         }
 }};
@@ -37,33 +35,46 @@ document.getElementById('6').addEventListener('click', () => {spotSelection(6)})
 document.getElementById('7').addEventListener('click', () => {spotSelection(7)});
 document.getElementById('8').addEventListener('click', () => {spotSelection(8)});
 document.getElementById('9').addEventListener('click', () => {spotSelection(9)});
+
 document.getElementById('resetBtn').addEventListener('click', () =>{   
     for(let i = 1; i < Object.keys(gameGrid).length+1; i++){
         gameGrid[i] = '';
         document.getElementById(i).innerHTML = '';
-        
-    }console.log(gameGrid)
+        turnNum = 1;
+        turn = X
+        playerBg(turn)
+        clickEvent = 0;
+        gameStatus = 'play'
+        document.getElementById('messageDis').innerHTML = 'Play again'
+    }
 })
 function GameOver() {
-    if(gameGrid[1]===gameGrid[2]&&gameGrid[2]===gameGrid[3]&&gameGrid[1]===gameGrid[3]||
-       gameGrid[4]===gameGrid[5]&&gameGrid[5]===gameGrid[6]&&gameGrid[4]===gameGrid[6]||
-       gameGrid[7]===gameGrid[8]&&gameGrid[8]===gameGrid[9]&&gameGrid[7]===gameGrid[9]||
-       gameGrid[1]===gameGrid[5]&&gameGrid[5]===gameGrid[9]&&gameGrid[1]===gameGrid[9]||
-       gameGrid[3]===gameGrid[5]&&gameGrid[5]===gameGrid[7]&&gameGrid[3]===gameGrid[7]||
-       gameGrid[1]===gameGrid[4]&&gameGrid[4]===gameGrid[7]&&gameGrid[1]===gameGrid[7]||
-       gameGrid[5]===gameGrid[2]&&gameGrid[5]===gameGrid[8]&&gameGrid[2]===gameGrid[8]||
-       gameGrid[3]===gameGrid[6]&&gameGrid[6]===gameGrid[9]&&gameGrid[3]===gameGrid[9]) {
-        return 'win'
-       }else if(turnNum == 9){
+    if(turnNum > 4) {
+        if(gameGrid[1]===gameGrid[2]&&gameGrid[2]===gameGrid[3]&&gameGrid[1]===gameGrid[3]||
+        gameGrid[4]===gameGrid[5]&&gameGrid[5]===gameGrid[6]&&gameGrid[4]===gameGrid[6]||
+        gameGrid[7]===gameGrid[8]&&gameGrid[8]===gameGrid[9]&&gameGrid[7]===gameGrid[9]||
+        gameGrid[1]===gameGrid[5]&&gameGrid[5]===gameGrid[9]&&gameGrid[1]===gameGrid[9]||
+        gameGrid[3]===gameGrid[5]&&gameGrid[5]===gameGrid[7]&&gameGrid[3]===gameGrid[7]||
+        gameGrid[1]===gameGrid[4]&&gameGrid[4]===gameGrid[7]&&gameGrid[1]===gameGrid[7]||
+        gameGrid[5]===gameGrid[2]&&gameGrid[5]===gameGrid[8]&&gameGrid[2]===gameGrid[8]||
+        gameGrid[3]===gameGrid[6]&&gameGrid[6]===gameGrid[9]&&gameGrid[3]===gameGrid[9]){
+            return 'win'
+        }else{
+            return 'play'
+        }
+        
+    }else if(turnNum == 9){
         return 'tie'
-       } 
-       else {
+    } 
+    else {
         return 'play'
-       }
+    }
 }
+    
+    
 function newTurn(){
-    gameStatus = GameOver();
-
+    gameStatus = GameOver(); 
+    
     if(gameStatus==='win'){
         switch(turn){
             case X:
@@ -78,8 +89,10 @@ function newTurn(){
     }
     else{
         if(turn === X){
+            turnNum += 1;
             return(turn = O);
         }else {
+            turnNum += 1;
             return(turn = X);
         }
     }
@@ -87,55 +100,29 @@ function newTurn(){
 function playerBg(turn){
     if(turn === X){
         document.getElementById('p1Display').classList.add("text-bg-primary");
+        document.getElementById('p2Display').classList.remove("text-bg-primary");
     }else if(turn === O){
         document.getElementById('p2Display').classList.add("text-bg-primary");
+        document.getElementById('p1Display').classList.remove("text-bg-primary");
     }
 }
+
 playerBg(turn)
 
-
-class Game {}
-    constructor();{
-    }
-    start()
-    while(gameStatus != 'tie'||gameStatus != 'win') {
-        document.getElementById('messageDis').innerHTML = `It's player ${turn} turn.`;
-        document.getElementById('mTable').addEventListener('click', () =>{
-            newTurn();
-
-        });
-        
-
-
-    }
-
-let game = new Game;
-game.start()
-/*class Menu {
-    constructor(){
+class Game {
+    constructor() {
     }
     start(){
-        let mainDis = document.getElementById('messageDis')
-        mainDis.innerHTML = "Player 1 to start."
         document.getElementById('mTable').addEventListener('click', () =>{
-            turnNum += 1;
-            turn = O
-            mainDis.innerHTML = '';
-        });
-        while(turnNum < 9) {
-            if(turn == X){
-                mainDis.innerHTML = 'Player 1 to select'
-                document.getElementById('mTable').addEventListener('click', () => {
-                    turn = O;
-                    turnNum += 1;
-                })
-            }else {
-                mainDis.innerHTML = 'Player 2 to select';
-                document.getElementById('mTable').addEventListener('click', () => {
-                    turn = X;
-                    turnNum += 1;
-                })
-            }
-        }
-    }
-}*/
+            clickEvent += 1;
+            console.log(clickEvent)
+            while(clickEvent===turnNum){
+            if(gameStatus=='tie'||gameStatus=='win'){
+                break
+            }else{
+                newTurn();
+                playerBg(turn);
+                console.log(gameStatus);
+        }}})}};
+let game = new Game;
+game.start();
