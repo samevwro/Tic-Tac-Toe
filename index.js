4e19a6ff678e0942fb37181e38027e59d1701fdf
Sam <samevwro@gmail.com>
2025-05-14 00:02:59 -0600

    laptop push

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 var X = 1;
 var O = 2;
 let turn = X;
-let turnNum = 1
+let turnNum = 1;
+let gameStatus = '';
+
 const XSpot = '❌';
 const OSpot = '⭕';
 
@@ -25,6 +27,7 @@ function spotSelection(id) {
             gameGrid[id] = O;
         }
 }};
+//Grid Click Detector
 document.getElementById('1').addEventListener('click', () => {spotSelection(1)});
 document.getElementById('2').addEventListener('click', () => {spotSelection(2)});
 document.getElementById('3').addEventListener('click', () => {spotSelection(3)});
@@ -41,7 +44,74 @@ document.getElementById('resetBtn').addEventListener('click', () =>{
         
     }console.log(gameGrid)
 })
-class Menu {
+function GameOver() {
+    if(gameGrid[1]===gameGrid[2]&&gameGrid[2]===gameGrid[3]&&gameGrid[1]===gameGrid[3]||
+       gameGrid[4]===gameGrid[5]&&gameGrid[5]===gameGrid[6]&&gameGrid[4]===gameGrid[6]||
+       gameGrid[7]===gameGrid[8]&&gameGrid[8]===gameGrid[9]&&gameGrid[7]===gameGrid[9]||
+       gameGrid[1]===gameGrid[5]&&gameGrid[5]===gameGrid[9]&&gameGrid[1]===gameGrid[9]||
+       gameGrid[3]===gameGrid[5]&&gameGrid[5]===gameGrid[7]&&gameGrid[3]===gameGrid[7]||
+       gameGrid[1]===gameGrid[4]&&gameGrid[4]===gameGrid[7]&&gameGrid[1]===gameGrid[7]||
+       gameGrid[5]===gameGrid[2]&&gameGrid[5]===gameGrid[8]&&gameGrid[2]===gameGrid[8]||
+       gameGrid[3]===gameGrid[6]&&gameGrid[6]===gameGrid[9]&&gameGrid[3]===gameGrid[9]) {
+        return 'win'
+       }else if(turnNum == 9){
+        return 'tie'
+       } 
+       else {
+        return 'play'
+       }
+}
+function newTurn(){
+    gameStatus = GameOver();
+
+    if(gameStatus==='win'){
+        switch(turn){
+            case X:
+                document.getElementById('messageDis').innerHTML = 'Player 1 IS THE WINNER!!!'
+                break;
+            case O:
+                document.getElementById('messageDis').innerHTML = 'Player 2 IS THE WINNER!!!'
+                break;
+        }
+    }else if(gameStatus ==='tie'){
+        document.getElementById('messageDis').innerHTML = 'Game is a tie.';
+    }
+    else{
+        if(turn === X){
+            return(turn = O);
+        }else {
+            return(turn = X);
+        }
+    }
+}
+function playerBg(turn){
+    if(turn === X){
+        document.getElementById('p1Display').classList.add("text-bg-primary");
+    }else if(turn === O){
+        document.getElementById('p2Display').classList.add("text-bg-primary");
+    }
+}
+playerBg(turn)
+
+
+class Game {}
+    constructor();{
+    }
+    start()
+    while(gameStatus != 'tie'||gameStatus != 'win') {
+        document.getElementById('messageDis').innerHTML = `It's player ${turn} turn.`;
+        document.getElementById('mTable').addEventListener('click', () =>{
+            newTurn();
+
+        });
+        
+
+
+    }
+
+let game = new Game;
+game.start()
+/*class Menu {
     constructor(){
     }
     start(){
@@ -68,4 +138,4 @@ class Menu {
             }
         }
     }
-}
+}*/
